a13dae5c7a1ff2565f43b2e2fb33607e2b529319
Matt Millar <matthew.millar@madetech.com>
2019-10-24 12:30:48 +0100

    Add id to customer creation response

--- src/Gateways/CreateCustomer.js
+++ src/Gateways/CreateCustomer.js
@@ -1,17 +1,19 @@
 function CreateCustomer(data, cb){
     console.log(data)
-    fetch('http://localhost:3010/customers',{
+    fetch('http://localhost:3010/customers', {
         method: 'POST',
         mode: 'cors',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({customers: data})
     })
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(myJson) {
-            cb(null, myJson);
-        });
+    .then(async function(response) {
+        const json = await response.json();
+        json.customer.id = response.url.split("/").pop();
+        return json;
+    })
+    .then(function(myJson) { 
+        cb(null, myJson);
+    });
 }
 
 export default CreateCustomer
