d072374244ad241c3a03c0699c07a2e171b55e36
Matt Millar <matthew.millar@madetech.com>
2019-10-29 11:50:05 +0000

    Update circleci config
    Added babel-eslint package
    
    
    Disable linting in circleci for now
    
    
    .
    
    
    Use npm in circleci
    
    
    .

--- src/lib/Cookie.js
+++ src/lib/Cookie.js
@@ -1,21 +1,21 @@
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 import jwt from 'jsonwebtoken';
 
-export const isLoggedIn = function(){
-    const hackneyToken = Cookies.get('hackneyToken')
-    if(hackneyToken){
-        let payload = jwt.decode(hackneyToken);
-        if(payload.iss === 'Hackney'){
-            return true
-        }
+export const isLoggedIn = function() {
+  const hackneyToken = Cookies.get('hackneyToken');
+  if (hackneyToken) {
+    let payload = jwt.decode(hackneyToken);
+    if (payload.iss === 'Hackney') {
+      return true;
     }
-    return false;
-}
+  }
+  return false;
+};
 
-export const saveToken = function(token){
-    Cookies.set('hackneyToken', token, { expires: 7 })
-}
+export const saveToken = function(token) {
+  Cookies.set('hackneyToken', token, { expires: 7 });
+};
 
-export const hackneyToken = function(token){
-    return Cookies.get('hackneyToken');
-}
+export const hackneyToken = function() {
+  return Cookies.get('hackneyToken');
+};
