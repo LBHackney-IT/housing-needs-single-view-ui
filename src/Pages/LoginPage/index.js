f067ecca5d021b1eb98163930513b11b492fd076
Matt Millar <matthew.millar@madetech.com>
2019-10-29 16:53:43 +0000

    Add correct redirect uri to login link

--- src/Pages/LoginPage/index.js
+++ src/Pages/LoginPage/index.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 export default class SelectPage extends Component {
   render() {
+    const redirect_uri = `${window.location.protocol}//${window.location.host}/callback&token_in_query=true`;
+
     return (
       <div className="loginPage">
         <h1>Please log in</h1>
         <a
-          href={`https://lbh-google-auth.herokuapp.com/auth?redirect_uri=${process.env.REACT_APP_HN_API_URL}/callback&token_in_query=true`}
+          href={`https://lbh-google-auth.herokuapp.com/auth?redirect_uri=${redirect_uri}`}
         >
           Log in with Google
         </a>
