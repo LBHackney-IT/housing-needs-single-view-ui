7214dcd7b7580326d0e77e1a59568da128229f9e
Matt Millar <matthew.millar@madetech.com>
2019-11-05 12:08:12 +0000

    Redirect from login page if logged in

--- src/Pages/LoginPage/index.js
+++ src/Pages/LoginPage/index.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { isLoggedIn } from '../../lib/Cookie';
 
 export default class LoginPage extends Component {
   render() {
+    if (isLoggedIn() === true) {
+      return <Redirect to="/" />;
+    }
+
     const redirect_uri = `${window.location.protocol}//${window.location.host}`;
 
     return (
-      <div className="loginPage">
-        <h1>Please log in</h1>
-        <a
-          href={`https://auth.hackney.gov.uk/auth?redirect_uri=${redirect_uri}`}
-        >
-          Log in with Google
-        </a>
+      <div className="lbh-container">
+        <div className="loginPage">
+          <h1>Please log in</h1>
+          <a
+            href={`https://auth.hackney.gov.uk/auth?redirect_uri=${redirect_uri}`}
+          >
+            Log in with Google
+          </a>
+        </div>
       </div>
     );
   }
