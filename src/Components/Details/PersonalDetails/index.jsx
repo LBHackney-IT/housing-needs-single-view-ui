b14f6ba03856be5426df7df309e143c43618dfac
Matt Millar <matthew.millar@madetech.com>
2019-11-11 09:58:39 +0000

    Fixed issue with null name details / dob

--- src/Components/Details/PersonalDetails/index.jsx
+++ src/Components/Details/PersonalDetails/index.jsx
@@ -8,14 +8,17 @@ export default class PersonalDetails extends Component {
     return (
       <div className="details__left-column__item">
         <h1>
-          {customer.name[0].title} {customer.name[0].first}{' '}
-          {customer.name[0].last}
+          {customer.name && customer.name[0].title}{' '}
+          {customer.name && customer.name[0].first}{' '}
+          {customer.name && customer.name[0].last}
         </h1>
         <table>
           <tbody>
             <tr>
               <td>Date of birth:</td>
-              <td>{moment(customer.dob[0]).format('DD/MM/YYYY')}</td>
+              <td>
+                {customer.dob && moment(customer.dob[0]).format('DD/MM/YYYY')}
+              </td>
             </tr>
             <tr>
               <td>National insurance no:</td>
