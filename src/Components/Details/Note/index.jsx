efaa0008be5da13bcac7c130f3b14a9a4f01ffe6
Ben Pirt <ben@pirt.co.uk>
2019-12-18 09:38:59 +0000

    Handle multiple sources for notes and force wrapping if they have long lines

--- src/Components/Details/Note/index.jsx
+++ src/Components/Details/Note/index.jsx
@@ -15,13 +15,13 @@ export default class Note extends Component {
           <p>
             <strong>{note.title}</strong>
           </p>
-          <p>{note.text}</p>
+          <p style={{overflowWrap: 'break-word', maxWidth: '350px'}}>{note.text}</p>
         </td>
         <td key="sys">
           <p>
             <strong>{note.user}</strong>
           </p>
-          <p>{note.system}</p>
+          <p>{ note.system.join ? note.system.join(', ') : note.system}</p>
         </td>
       </tr>
     );
