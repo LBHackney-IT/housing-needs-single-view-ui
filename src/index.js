7a7f86d994f628da30dd70097e1a541d1dff4f2e
Elena <elena@madetech.com>
2020-02-10 14:07:10 +0000

    implementing sentry

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
+
+if (process.env.ENV === 'staging' || process.env.ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.ENV
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
