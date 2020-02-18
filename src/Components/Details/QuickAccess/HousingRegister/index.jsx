d09dedf9f919de51d9e024b2b4eb81175272af96
Kat <katrina@madetech.com>
2020-02-18 14:39:55 +0000

    WIP

--- src/Components/Details/QuickAccess/HousingRegister/index.jsx
+++ src/Components/Details/QuickAccess/HousingRegister/index.jsx
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
+import Modal from '../../../Modal';
 
 export default class HousingRegister extends Component {
+  housingRegisterRecord(t) {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>Application Ref:</td>
+            <td>{t.applicationRef}</td>
+          </tr>
+          <tr>
+            <td>Bidding no:</td>
+            <td>{t.biddingNo}</td>
+          </tr>
+          <tr>
+            <td>Band:</td>
+            <td>{t.band}</td>
+          </tr>
+          <tr>
+            <td>Date:</td>
+            <td>{t.startDate}</td>
+          </tr>
+          <br />
+        </tbody>
+      </table>
+    );
+  }
+
   render() {
     const { customer } = this.props;
 
@@ -23,19 +50,21 @@ export default class HousingRegister extends Component {
             </tr>
           </tbody>
         </table>
-        {/* <div className="quick-access__item__links">
+        <div className="quick-access__item__links">
           <ul>
             <li>
               <Modal trigger={<a href="#/">More details</a>}>
-                <h3>...</h3>
+                <h3>Housing Register Information</h3>
                 <br />
                 <table>
-                  <tbody></tbody>
+                  {customer.housingRegister.map(t =>
+                    this.housingRegisterRecord(t)
+                  )}
                 </table>
               </Modal>
             </li>
           </ul>
-        </div> */}
+        </div>
       </div>
     );
   }
