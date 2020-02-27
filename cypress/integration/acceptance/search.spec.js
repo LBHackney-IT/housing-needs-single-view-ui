57b356c49542c7bd5202c65ffc2ab098cfddcc4a
Elena <elena@madetech.com>
2020-02-27 12:11:53 +0000

    add endpoints for connecting records and viewing customer

--- cypress/integration/acceptance/search.spec.js
+++ cypress/integration/acceptance/search.spec.js
@@ -12,27 +12,46 @@ describe('Search', () => {
       domain: 'localhost'
     });
   };
-
-  it('Logs into Single View with a valid token', () => {
-    setHackneyCookie(true);
-    cy.visit('http://localhost:3001');
-    cy.contains('Welcome to Single View');
+  describe('When not logged in', () => {
+    it('Does not log into Single View with an invalid token', () => {
+      setHackneyCookie(false);
+      cy.visit('http://localhost:3001');
+      cy.contains('Please log in');
+    });
   });
 
-  it('Does not log into Single View with an invalid token', () => {
-    setHackneyCookie(false);
-    cy.visit('http://localhost:3001');
-    cy.contains('Please log in');
-  });
+  describe('When logged in', () => {
+    beforeEach(() => {
+      setHackneyCookie(true);
+    });
 
-  it('makes a fake request', () => {
-    setHackneyCookie(true);
-    cy.visit('http://localhost:3001');
-    cy.get('.govuk-input:first').type('John');
-    cy.get('.govuk-input:last')
-      .type('Smith')
-      .type('{enter}');
-    cy.contains('Customers with matching details');
-    cy.contains('Rick Sanchez');
+    it('Logs into Single View with a valid token', () => {
+      cy.visit('http://localhost:3001');
+      cy.contains('Welcome to Single View');
+    });
+
+    it('Verify that relevant results are returned', () => {
+      cy.get('.govuk-input:first').type('Wednesday');
+      cy.get('.govuk-input:last')
+        .type('Adams')
+        .type('{enter}');
+      cy.contains('Customers with matching details');
+      cy.contains('Wednesday Adams');
+    });
+
+    it('Join relevant records', () => {
+      cy.get('.groupedTable').first()
+        .find('tr')
+        .then(result => {
+          result.each((_, otherThing) => {
+            otherThing.click();
+          });
+        });
+      cy.contains('Connect records').click();
+    });
+
+    it('User sees customer info', () => {
+      cy.get('h1').should('contain', 'Miss Wednesday Adams');
+    });
   });
 });
