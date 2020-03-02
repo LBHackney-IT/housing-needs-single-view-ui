b37b2b9916be2ea5732b56236fc20ab1a7c64362
Ben Pirt <ben@pirt.co.uk>
2020-03-02 08:34:01 +0000

    Add sms tool to prototypes list

--- src/app/Components/Details/PrototypesLink/index.jsx
+++ src/app/Components/Details/PrototypesLink/index.jsx
@@ -21,13 +21,17 @@ export default function PrototypesLink(props){
         <Modal trigger={<button style={linkStyle}>Prototype testing</button>}>
           <h3>Prototype testing</h3>
           <h4>Vulnerability Snapshot:</h4>
-          <ul style={{'marginBottom':'10px'}}>
+          <ul style={{'margin':'10px 0'}}>
             <li><Link to={`/customers/${props.id}/vulnerabilities`}>Prototype 1: Free text</Link></li>
             <li><a href={`${process.env.REACT_APP_VULNERABILITIES_APP_URL}/customers/${customerId}`}>Prototype 2: Checklist</a></li>
           </ul>
-          <h4>Shared Plan:</h4>
-          <p>This person already has a PHP in Jigsaw. Please take a look before creating a Shared Plan</p>
-          <a href={`${process.env.REACT_APP_VULNERABILITIES_APP_URL}/customers/${customerId}/plan`}>Shared Plan</a>
+          <h4 style={{'margin':'10px 0'}}>Shared Plan:</h4>
+          { props.customer.housingNeeds.jigsawCaseId
+            ? <p>This person already has a PHP in Jigsaw. Please take a look before creating a Shared Plan</p>
+            : null }
+          <p><a href={`${process.env.REACT_APP_VULNERABILITIES_APP_URL}/customers/${customerId}/plan`}>Visit Shared Plan</a></p>
+          <h4 style={{'margin':'10px 0'}}>SMS Tool:</h4>
+          <p><a href={`${process.env.REACT_APP_VULNERABILITIES_APP_URL}/customers/${customerId}/plan`}>Visit SMS Tool</a></p>
         </Modal>
       </div>
     );
