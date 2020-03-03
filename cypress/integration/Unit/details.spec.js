8065347e5434e7971f73965b330c08c7d88fa615
Elena <elena@madetech.com>
2020-03-03 15:48:58 +0000

    search activities

--- cypress/integration/Unit/details.spec.js
+++ cypress/integration/Unit/details.spec.js
@@ -1,24 +1,55 @@
 /// <reference types="cypress" />
 import jwt from 'jsonwebtoken';
-describe('Documents', () => {
-  it('Displays title as a clickable link', () => {
-    const token = jwt.sign(
-      { groups: ['housingneeds-singleview-beta'] },
-      'a-secure-signature'
-    );
-    cy.setCookie('hackneyToken', token, {
+
+describe('Details Page', () => {
+  const setHackneyCookie = async isValidGroup => {
+    const group = isValidGroup
+      ? 'housingneeds-singleview-beta'
+      : 'some-other-group';
+    const token = jwt.sign({ groups: [group] }, 'a-secure-signature');
+    await cy.setCookie('hackneyToken', token, {
       url: 'http://localhost:3001',
       domain: 'localhost'
     });
-    cy.visit('http://localhost:3001/customers/5/view');
+  };
 
-    cy.get('.activity > table > tbody > tr > td > strong > a').each($el =>
-      cy
-        .wrap($el)
-        .should('contain', 'Document')
-        .and('not.contain', 'Note')
-        .and('not.contain', 'Academy')
-        .and('have.attr', 'href')
-    );
+  beforeEach(() => {
+    setHackneyCookie(true);
+    cy.visit('http://localhost:3001/customers/5/view');
+  });
+
+  describe('Search Activity', () => {
+
+    it('can filter by note tile', () => {
+      const testNoteTitle = 'Case Note';
+
+      cy.get('#search')
+        .should('be.visible')
+        .type(testNoteTitle);
+
+      cy.get('.activity > table > tbody > tr > :nth-child(2)')
+        .should('have.length', '4')
+        .each($el => cy.wrap($el).should('contain', testNoteTitle));
+    });
+
+    it('can filter by note text', () => {
+      const testNoteText = 'Change in Circs ICL';
+
+      cy.get('#search')
+        .should('be.visible')
+        .type(testNoteText);
+
+      cy.get('.activity > table > tbody > tr > :nth-child(2)')
+        .should('have.length', '1')
+        .each($el => cy.wrap($el).should('contain', testNoteText));
+    });
+  });
+
+  describe('Documents', () => {
+    it('Has a link', () => {
+      cy.get('tbody > tr > td > a').each($el =>
+        cy.wrap($el).should('contain', 'Document')
+      );
+    });
   });
 });
--- src/app/Components/Details/Activity/index.jsx
+++ src/app/Components/Details/Activity/index.jsx
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import Note from '../Note';
 
 export default class Activity extends Component {
+  state = {
+    notes: this.props.notes
+  };
+
+  searchNotes = event => {
+    this.setState({
+      notes: this.props.notes.filter(item => {
+        const byTitle =
+          item.title.toLowerCase().search(event.target.value.toLowerCase()) !==
+          -1;
+        const byText =
+          item.text.toLowerCase().search(event.target.value.toLowerCase()) !==
+          -1;
+        return byText || byTitle;
+      })
+    });
+  };
+
   render() {
-    const { notes } = this.props;
+    const notes = this.state.notes;
 
     return (
       <div className="activity">
+        <input type="text" id="search" placeholder="Search" onChange={this.searchNotes} />
+
         <h2>Activity</h2>
         {notes.length > 0 ? (
           <table>
