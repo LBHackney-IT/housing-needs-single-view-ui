552906ad8f9989d4b9d3c4d88c7a09724373e307
Elena <elena@madetech.com>
2020-03-03 16:43:05 +0000

    basic filters for type

--- src/app/Components/Details/Activity/index.jsx
+++ src/app/Components/Details/Activity/index.jsx
@@ -3,20 +3,34 @@ import Note from '../Note';
 
 export default class Activity extends Component {
   state = {
-    notes: this.props.notes
+    notes: this.props.notes,
+    filter: null
+  };
+
+  filterNotes = event => {
+    const filter = event.target.value;
+    if (filter == 'note' || filter == 'document') {
+      this.setState({
+        filter: event.target.value,
+        notes: this.props.notes.filter(note => note.type == event.target.value)
+      });
+    }
   };
 
   searchNotes = event => {
+    const notes = this.state.filter ? this.state.notes : this.props.notes;
+
+    const foundNotes = notes.filter(item => {
+      const byTitle =
+        item.title.toLowerCase().search(event.target.value.toLowerCase()) !==
+        -1;
+      const byText =
+        item.text.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+      return byText || byTitle;
+    });
+
     this.setState({
-      notes: this.props.notes.filter(item => {
-        const byTitle =
-          item.title.toLowerCase().search(event.target.value.toLowerCase()) !==
-          -1;
-        const byText =
-          item.text.toLowerCase().search(event.target.value.toLowerCase()) !==
-          -1;
-        return byText || byTitle;
-      })
+      notes: foundNotes
     });
   };
 
@@ -25,7 +39,31 @@ export default class Activity extends Component {
 
     return (
       <div className="activity">
-        <input type="text" id="search" placeholder="Search" onChange={this.searchNotes} />
+        <input
+          type="text"
+          id="search"
+          placeholder="Search"
+          onChange={this.searchNotes}
+        />
+
+        <input
+          type="radio"
+          id="note"
+          value="note"
+          name="filter"
+          onClick={this.filterNotes}
+          hidden={true}
+        />
+        <label htmlFor="note">All Notes</label>
+        <input
+          type="radio"
+          value="document"
+          id="document"
+          name="filter"
+          onClick={this.filterNotes}
+          hidden={true}
+        />
+        <label htmlFor="document">All Documents</label>
 
         <h2>Activity</h2>
         {notes.length > 0 ? (
