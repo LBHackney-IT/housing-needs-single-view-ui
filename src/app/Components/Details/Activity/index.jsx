0982c86ef54af2389f47cc1fc7a210dee47f2a36
Elena <elena@madetech.com>
2020-03-04 15:24:41 +0000

    nice styles

--- src/app/Components/Details/Activity/index.jsx
+++ src/app/Components/Details/Activity/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Note from '../Note';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default class Activity extends Component {
   state = {
@@ -7,18 +9,32 @@ export default class Activity extends Component {
     filter: null
   };
 
-  filterNotes = event => {
-    const filter = event.target.value;
-    if (filter == 'note' || filter == 'document') {
-      this.setState({
-        filter: event.target.value,
-        notes: this.props.notes.filter(note => note.type == event.target.value)
-      });
+  setFilter = event => {
+    const type = event.target.value;
+    if (type === 'note' || type === 'document') {
+      if (type === this.state.filter) {
+        event.target.checked = false;
+        this.setState({
+          filter: null,
+          notes: this.props.notes
+        });
+      } else {
+        this.setState({
+          filter: type,
+          notes: this.filteredNotesByType(type)
+        });
+      }
     }
   };
 
+  filteredNotesByType = type => {
+    return this.props.notes.filter(note => note.type == type);
+  };
+
   searchNotes = event => {
-    const notes = this.state.filter ? this.state.notes : this.props.notes;
+    const notes = this.state.filter
+      ? this.filteredNotesByType(this.state.filter)
+      : this.props.notes;
 
     const foundNotes = notes.filter(item => {
       const byTitle =
@@ -39,33 +55,46 @@ export default class Activity extends Component {
 
     return (
       <div className="activity">
-        <input
-          type="text"
-          id="search"
-          placeholder="Search"
-          onChange={this.searchNotes}
-        />
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds">
+            <h2>Activity</h2>
+          </div>
 
-        <input
-          type="radio"
-          id="note"
-          value="note"
-          name="filter"
-          onClick={this.filterNotes}
-          hidden={true}
-        />
-        <label htmlFor="note">All Notes</label>
-        <input
-          type="radio"
-          value="document"
-          id="document"
-          name="filter"
-          onClick={this.filterNotes}
-          hidden={true}
-        />
-        <label htmlFor="document">All Documents</label>
+          <div className="govuk-grid-column-one-third">
+            <div className="activity__search">
+              <input
+                type="text"
+                placeholder="Search"
+                className="govuk-input"
+                onChange={this.searchNotes}
+              />
+              <button onClick={this.showFilters}>
+                <FontAwesomeIcon icon={faSearch} />
+              </button>
+              {/*<button className="govuk-button lbh-button">x</button>*/}
+              <div className="govuk-visually-hidden">
+                <input
+                  type="radio"
+                  id="note"
+                  value="note"
+                  name="filter"
+                  onClick={this.setFilter}
+                />
+                <label htmlFor="note">All Notes</label>
+                <br />
+                <input
+                  type="radio"
+                  value="document"
+                  id="document"
+                  name="filter"
+                  onClick={this.setFilter}
+                />
+                <label htmlFor="document">All Documents</label>
+              </div>
+            </div>
+          </div>
+        </div>
 
-        <h2>Activity</h2>
         {notes.length > 0 ? (
           <table>
             <tbody>
