7e26d1e0943f5680d2c404f89cd428950bc74410
el <elena@madetech.com>
2020-03-09 16:39:53 +0000

    Search and filter activity (#18)
    * search activities
    
    * restore document link tests
    
    * basic filters for type
    
    * nice styles
    
    * less confusion
    
    * Refactor activity search
    
    * functioning search
    
    * font awesome packacge.json
    
    * tests
    
    * scroll search into view
    
    * testy tests
    
    * fixing failing tests
    
    * testing tests on circleci
    
    * x clears the searchterm too
    
    * scroll filters into view
    
    * filter doc test
    
    * force click filters
    
    * test filters
    
    * delete unwanted files
    
    * Update cypress/integration/Unit/details.spec.js
    
    Co-authored-by: Kat <katrina@madetech.com>
    Co-authored-by: Bogdan <bogdan@madetech.com>

--- cypress/integration/Unit/details.spec.js
+++ cypress/integration/Unit/details.spec.js
@@ -11,6 +11,92 @@ describe('Details Page', () => {
       domain: 'localhost'
     });
   };
+
+  beforeEach(() => {
+    setHackneyCookie(true);
+    cy.visit('http://localhost:3001/customers/5/view');
+    cy.contains('Activity').scrollIntoView();
+  });
+
+  describe('Search Activity', () => {
+    it('can search by note title', () => {
+      const testNoteTitle = 'Case Note';
+
+      cy.get('#search')
+        .scrollIntoView()
+        .should('be.visible')
+        .type(testNoteTitle, { force: true });
+
+      cy.get('.activity > table > tbody > tr > :nth-child(2)')
+        .should('have.length', '4')
+        .each($el => cy.wrap($el).should('contain', testNoteTitle));
+    });
+
+    it('can search by note text', () => {
+      const testNoteText = 'Change in Circs ICL';
+
+      cy.get('#search')
+        .scrollIntoView()
+        .should('be.visible')
+        .type(testNoteText, { force: true });
+
+      cy.get('.activity > table > tbody > tr > :nth-child(2)')
+        .should('have.length', '1')
+        .each($el => cy.wrap($el).should('contain', testNoteText));
+    });
+
+    describe('filters', () => {
+      beforeEach(() => {
+        cy.get('.activity__search button')
+          .should('be.visible')
+          .click({ force: true });
+
+        cy.get('.activity__filters')
+          .should('be.visible')
+          .children()
+          .should('have.length', '2');
+      });
+
+      it('can filter by notes', () => {
+        cy.get('.activity__filters > :nth-child(1)')
+          .should('be.visible')
+          .and('contain', 'All Notes')
+          .click({ force: true });
+
+        cy.get('.selectedFilter')
+          .should('be.visible')
+          .and('contain', 'All Notes');
+
+        cy.get('.activity > table > tbody > tr > :nth-child(2)')
+          .should('have.length', '34')
+          .each($el => cy.wrap($el).should('contain', 'Note'));
+
+        cy.get('.activity__search button').click({ force: true });
+
+        cy.get('.selectedFilter').should('not.be.visible');
+      });
+
+      it('can filter by documents', () => {
+        cy.get('.activity__filters > :nth-child(2)')
+          .should('be.visible')
+          .and('contain', 'All Documents')
+          .click({ force: true });
+
+        cy.get('.selectedFilter')
+          .should('be.visible')
+          .and('contain', 'All Documents');
+
+        cy.get('.activity > table > tbody > tr > :nth-child(2)')
+          .should('have.length', '42')
+          .each($el => cy.wrap($el).should('contain', 'Document'));
+
+        cy.get('.activity__search button').click({ force: true });
+
+        cy.get('.selectedFilter').should('not.be.visible');
+      });
+    });
+  });
+
   describe('Documents', () => {
     it('Displays title as a clickable link', () => {
       setHackneyCookie(true);
