4a16e98ba69198d1443d00585a7de9a2d6cc1e98
Kat <katrina@madetech.com>
2020-03-11 13:57:58 +0000

    Adds hotjar, addes REACT_APP to env vars

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './app/App';
 import * as serviceWorker from './app/serviceWorker';
 import * as Sentry from '@sentry/browser';
+import { hotjar } from 'react-hotjar';
 
-if (process.env.ENV === 'staging' || process.env.ENV === 'production') {
+if (
+  process.env.REACT_APP_ENV === 'staging' ||
+  process.env.REACT_APP_ENV === 'production'
+) {
   Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    environment: process.env.ENV
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    environment: process.env.REACT_APP_ENV
   });
+}
+if (process.env.REACT_APP_ENV === 'production') {
+  hotjar.initialize(process.env.REACT_APP_HOTJAR_ID, 6);
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
