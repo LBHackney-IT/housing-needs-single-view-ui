2e4f5835720f8098dcdc2ba7609832ba6ec34598
Bogdan <bogdan@madetech.com>
2020-03-25 11:40:07 +0000

    adding privacy notice to the log in page

--- src/app/Pages/LoginPage/index.jsx
+++ src/app/Pages/LoginPage/index.jsx
@@ -19,6 +19,23 @@ export default class LoginPage extends Component {
           >
             Log in with Google
           </a>
+          <div className="privacy-notice">
+            <h3>Privacy Notice</h3>
+            <p>
+              We use Google Analytics and Hotjar to collect information about
+              how you use this site. We do this to make sure it’s meeting your
+              needs and to understand how we can make the website work better.
+            </p>
+            <p>
+              Google Analytics stores information about what pages on this site
+              you visit, how long you are on the site, how you got here and what
+              you click on while you are here.
+            </p>
+            <p>
+              Hotjar sets cookies to help us track behaviour across pages and to
+              control visitor polls.
+            </p>
+          </div>
         </div>
       </div>
     );
