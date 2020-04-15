f42670e2fb3fccedbc8f4b365b747f58cb6d0bc2
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2020-04-15 15:24:59 +0100

    Feedback link and page titles
    Add page titles, set lang to en-gb.
    Add "opens in a new tab" for screen readers and icon for visuals.

--- src/app/Components/Phase/index.jsx
+++ src/app/Components/Phase/index.jsx
@@ -4,6 +4,9 @@ export default class Phase extends Component {
   render() {
     return (
       <div className="govuk-phase-banner lbh-phase-banner lbh-container">
+        <div hidden>
+          <span id="new-tab">Opens in a new tab</span>
+        </div>
         <p className="govuk-phase-banner__content">
           <strong className="govuk-tag govuk-phase-banner__content__tag">
             Alpha
@@ -14,8 +17,10 @@ export default class Phase extends Component {
               href="https://docs.google.com/forms/d/e/1FAIpQLSdgiT3dktz647tu0T1MMAmoQwI0CvdIdIR6zQPB7Cyo0-mIqg/viewform"
               target="_blank"
               rel="noreferrer noopener"
+              aria-describedby="new-tab"
             >
               feedback
+              <img src="/assets/images/new-tab.png" alt="" width="10" />
             </a>{' '}
             will help us to improve it.
           </span>
