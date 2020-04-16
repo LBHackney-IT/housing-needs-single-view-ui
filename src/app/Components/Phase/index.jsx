0ef8ef5f325c19b518b286f18b5f77a3f4c644e2
kosiakkatrina <54268893+kosiakkatrina@users.noreply.github.com>
2020-04-16 15:44:19 +0100

    Change banner to beta

--- src/app/Components/Phase/index.jsx
+++ src/app/Components/Phase/index.jsx
@@ -7,24 +7,26 @@ export default class Phase extends Component {
         <div hidden>
           <span id="new-tab">Opens in a new tab</span>
         </div>
-        <p className="govuk-phase-banner__content">
-          <strong className="govuk-tag govuk-phase-banner__content__tag">
-            Alpha
-          </strong>
-          <span className="govuk-phase-banner__text">
-            This is a new service - your{' '}
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSdgiT3dktz647tu0T1MMAmoQwI0CvdIdIR6zQPB7Cyo0-mIqg/viewform"
-              target="_blank"
-              rel="noreferrer noopener"
-              aria-describedby="new-tab"
-            >
-              feedback
-              <img src="/assets/images/new-tab.png" alt="" width="10" />
-            </a>{' '}
-            will help us to improve it.
-          </span>
-        </p>
+        <div class="govuk-phase-banner  lbh-phase-banner lbh-container">
+          <p class="govuk-phase-banner__content">
+            <strong class="govuk-tag govuk-phase-banner__content__tag  lbh-tag">
+              Beta
+            </strong>
+            <span class="govuk-phase-banner__text">
+              This is our new website design - it's work in progress.{' '}
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSdgiT3dktz647tu0T1MMAmoQwI0CvdIdIR6zQPB7Cyo0-mIqg/viewform"
+                target="_blank"
+                rel="noreferrer noopener"
+                aria-describedby="new-tab"
+              >
+                Tell us what you think
+                <img src="/assets/images/new-tab.png" alt="" width="10" />
+              </a>{' '}
+              , your feedback will help us to improve it.
+            </span>
+          </p>
+        </div>
       </div>
     );
   }
