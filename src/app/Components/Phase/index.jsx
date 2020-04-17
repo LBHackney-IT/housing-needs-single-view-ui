bf9794380869c7ed8266eff7fece19dc142509ca
Kat <katrina@madetech.com>
2020-04-17 12:12:31 +0100

    wording

--- src/app/Components/Phase/index.jsx
+++ src/app/Components/Phase/index.jsx
@@ -13,7 +13,7 @@ export default class Phase extends Component {
               Beta
             </strong>
             <span className="govuk-phase-banner__text">
-              This is our new website design - it's work in progress.{' '}
+              This is work in progress.{' '}
               <a
                 href="https://docs.google.com/forms/d/e/1FAIpQLSdgiT3dktz647tu0T1MMAmoQwI0CvdIdIR6zQPB7Cyo0-mIqg/viewform"
                 target="_blank"
