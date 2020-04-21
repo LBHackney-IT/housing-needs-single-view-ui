754c224dcff95a79fbebb584c993ed80f3972b53
Matt Millar <matthew.millar@madetech.com>
2020-04-21 14:01:41 +0100

    Use hackney token when fetching notes for sms

--- src/app/Gateways/FetchCustomerNotes.js
+++ src/app/Gateways/FetchCustomerNotes.js
@@ -1,9 +1,12 @@
+import { hackneyToken } from '../lib/Cookie';
+
 export default async id => {
   const response = await fetch(
     `${process.env.REACT_APP_HN_API_URL}/customers/${id}/notes`,
     {
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${hackneyToken()}`
       }
     }
   );
