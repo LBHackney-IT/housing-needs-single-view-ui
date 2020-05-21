9aef0984e2801d9a8de25403b0639f10e4f2a157
Tom Davies <tom.davies@madetech.com>
2020-05-21 11:51:58 +0100

    Adds <SharedPlanList /> to list shared plans

---
+++ src/app/Components/Details/QuickAccess/SharedPlan/SharedPlansList/index.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import findSharedPlans from '../../../../../Gateways/FindSharedPlans';
+import styles from './SharedPlansList.module.scss';
+
+const SharedPlansList = ({ customerId }) => {
+  const [state, setState] = useState({
+    plans: [],
+    loading: true,
+    error: null
+  });
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const plans = await findSharedPlans({ customerId });
+        setState({ plans, loading: false });
+      } catch (error) {
+        setState({ error, loading: false });
+      }
+    };
+
+    fetch();
+  }, [customerId]);
+
+  if (state.loading) {
+    return null;
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <h4 className={styles.heading}>Existing plans</h4>
+      <ul>
+        {state.plans.map(({ id, location }) => (
+          <li key={id}>
+            <a href={location}>{id}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SharedPlansList;
---
+++ src/app/Gateways/FindSharedPlans.js
@@ -0,0 +1,24 @@
+import { hackneyToken } from '../lib/Cookie';
+
+export default async ({ customerId }) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_HN_API_URL}/customers/${customerId}/shared-plans`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${hackneyToken()}`
+      }
+    }
+  );
+
+  if (response.ok) {
+    const { planIds } = await response.json();
+
+    return planIds.map(id => ({
+      id,
+      location: `${process.env.REACT_APP_SHARED_PLAN_URL}/${id}`
+    }));
+  }
+
+  throw new Error(`Failed to find plans, status: ${response.status}`);
+};
