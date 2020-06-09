855c123b3ad4f0f00f37d347666046e65476bc29
Matt Millar <matthew.millar@madetech.com>
2020-06-09 08:31:03 +0100

    Remove redundant else

--- src/app/Components/PrivateRoute/index.js
+++ src/app/Components/PrivateRoute/index.js
@@ -7,9 +7,8 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
   if (isLoggedIn() === true) {
     if (window.location.hash) setFeatureFlag(window.location.hash.slice(1));
     return <Route {...rest} render={props => <Component {...props} />} />;
-  } else {
-    return <Route {...rest} render={() => <Redirect to="/login" />} />;
   }
+  return <Route {...rest} render={() => <Redirect to="/login" />} />;
 };
 
 export default PrivateRoute;
