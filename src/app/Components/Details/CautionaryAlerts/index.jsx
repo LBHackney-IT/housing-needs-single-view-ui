38fecfb2901c97be9845a35ecd1580c9ba531a57
Kat <katrina@madetech.com>
2020-09-16 09:25:54 +0100

    Add cypress test and fix styling

--- src/app/Components/Details/CautionaryAlerts/index.jsx
+++ src/app/Components/Details/CautionaryAlerts/index.jsx
@@ -6,8 +6,8 @@ const CautionaryAlerts = ({ label, alert }) => {
     <div className={styles['alert-item']}>
       <h3>{label}</h3>
       <ul>
-        <li>{alert.description}</li>
-        <li>Added: {alert.startDate}</li>
+        <li data-testid="alert-description">{alert.description}</li>
+        <li data-testid="alert-date">Added: {alert.startDate}</li>
       </ul>
     </div>
   );
