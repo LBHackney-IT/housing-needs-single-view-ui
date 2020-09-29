1453f7214ebc0963537fc4f5279132cb3477aa85
Bogdan <bogdan@madetech.com>
2020-09-29 14:01:02 +0100

    Add pagination for rent transactions

--- src/app/Components/Details/RentTransactions/index.jsx
+++ src/app/Components/Details/RentTransactions/index.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
+import './index.scss';
 
 export default class RentTransactions extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      transactionsDisplayed: 10
+      transactionsDisplayed: 20,
+      pageNumber: 0
     };
   }
 
   render() {
-    const transactions = this.props.transactions
-      .slice(0, this.state.transactionsDisplayed)
-      .map(transaction => {
-        return transaction;
-      });
+    let resultsRange = [];
 
-    if (transactions.length > 0) {
+    const totalTransactions = this.props.transactions.length;
+    const firstPageItem =
+      this.state.pageNumber * parseInt(this.state.transactionsDisplayed);
+    const remainingTransactions = totalTransactions - firstPageItem;
+    const lastPageItem =
+      remainingTransactions < parseInt(this.state.transactionsDisplayed)
+        ? remainingTransactions + firstPageItem
+        : firstPageItem + parseInt(this.state.transactionsDisplayed);
+
+    for (let i = firstPageItem; i < lastPageItem; i++) {
+      resultsRange.push(i);
+    }
+
+    if (totalTransactions > 0) {
       return (
         <>
           <h2>Rent</h2>
@@ -40,30 +51,60 @@ export default class RentTransactions extends Component {
               </tr>
             </thead>
             <tbody className="govuk-table__body">
-              {transactions.map((transaction, index) => (
+              {resultsRange.map(index => (
                 <tr
                   className="govuk-table__row"
                   key={`${index}-key`}
                   data-testid={`transaction-row-${index}`}
                 >
-                  <td className="govuk-table__cell">{transaction.date}</td>
                   <td className="govuk-table__cell">
-                    {transaction.description}
+                    {this.props.transactions[index].date}
+                  </td>
+                  <td className="govuk-table__cell">
+                    {this.props.transactions[index].description}
                   </td>
 
                   <td className="govuk-table__cell">
-                    {transaction.out.replace('¤', '£') || ''}
+                    {this.props.transactions[index].out.replace('¤', '£') || ''}
                   </td>
                   <td className="govuk-table__cell">
-                    {transaction.in.replace('¤', '£') || ''}
+                    {this.props.transactions[index].in.replace('¤', '£') || ''}
                   </td>
                   <td className="govuk-table__cell">
-                    {transaction.balance.replace('¤', '£') || ''}
+                    {this.props.transactions[index].balance.replace('¤', '£') ||
+                      ''}
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
+          <div className="page-navigation">
+            {this.state.pageNumber > 0 && (
+              <button
+                className="govuk-button previous-page-button"
+                data-module="govuk-button"
+                onClick={() => {
+                  this.setState({ pageNumber: this.state.pageNumber - 1 });
+                }}
+              >
+                Previous page
+              </button>
+            )}
+            <span className="current-page-number">
+              Current page: {this.state.pageNumber + 1}
+            </span>
+            {remainingTransactions >= this.state.transactionsDisplayed && (
+              <button
+                className="govuk-button next-page-button"
+                data-module="govuk-button"
+                onClick={() => {
+                  this.setState({ pageNumber: this.state.pageNumber + 1 });
+                }}
+              >
+                Next page
+              </button>
+            )}
+          </div>
         </>
       );
     }
